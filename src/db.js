8f69f4c6983be255d5cb289afaf852f43cc80be1
Tu Nombre <villafuerte.mariajose046@gmail.com>
2024-03-18 20:12:33 -0600

    update: change of table name and some characteristics

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
-import conn from './conn.js'
+import conn from './conn.js';
 
 async function getAllPosts() {
   try {
-    const [rows] = await conn.query('SELECT * FROM recipe_posts');
+    const [rows] = await conn.query('SELECT * FROM books_review_posts');
     console.log('Rows from database:', rows); // Agregar este mensaje de registro
     return rows;
   } catch (error) {
@@ -11,32 +11,30 @@ async function getAllPosts() {
   }
 }
 
-async function createPost(title, content, author, category, tags) {
+async function createPost(title, content, author, genre) {
   const [result] = await conn.query(
-    'INSERT INTO recipe_posts (title, content, author, category, tags) VALUES (?, ?, ?, ?, ?)',
-    [title, author, category, content, tags],
-  )
-  return result
+    'INSERT INTO books_review_posts (title, content, author, genre) VALUES (?, ?, ?, ?)',
+    [title, content, author, genre],
+  );
+  return result;
 }
 
 async function getById(postId) {
-  const [result] = await conn.query('SELECT * FROM recipe_posts WHERE id = ?', [postId])
-  return result
+  const [result] = await conn.query('SELECT * FROM books_review_posts WHERE id = ?', [postId]);
+  return result;
 }
 
 async function deletebyID(postId) {
-  const [deleteResult] = await conn.query('DELETE FROM recipe_posts WHERE id = ?', [postId])
-  return deleteResult
+  const [deleteResult] = await conn.query('DELETE FROM books_review_posts WHERE id = ?', [postId]);
+  return deleteResult;
 }
 
-async function updatePostById(title, content, author, category, tags, postId) {
+async function updatePostById(title, content, author, genre, postId) {
   const [result] = await conn.query(
-    'UPDATE recipe_posts SET title = ?, content = ?, author = ?, category = ?, tags = ? WHERE id = ?',
-    [title, content, author, category, tags, postId],
-  )
-  return result
+    'UPDATE books_review_posts SET title = ?, content = ?, author = ?, genre = ? WHERE id = ?',
+    [title, content, author, genre, postId],
+  );
+  return result;
 }
 
-export {
-  createPost, getAllPosts, getById, deletebyID, updatePostById,
-}
+export { createPost, getAllPosts, getById, deletebyID, updatePostById };
--- src/index.js
+++ src/index.js
@@ -1,85 +1,79 @@
-import express from 'express'
-import cors from 'cors'
-import fs from 'fs'
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import {
-  getAllPosts, getById, createPost, updatePostById, deletebyID,
-} from './db.js'
+  getAllPosts,
+  getById,
+  createPost,
+  updatePostById,
+  deletebyID,
+} from './db.js';
 
-const app = express()
-const port = 3000
-app.use(express.json);
-app.use(cors())
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(cors());
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', async (req, res) => {
-  res.send('HELLO FROM MY API')
-})
-
-
-app.get('/mi', async (req, res) => {
-  try {
-    console.log(await getAllPosts())
-  } catch (error) {
-    console.error('Error al obtener los posts:', error)
-    res.status(500).send('Error en el servidor')
-  }
-})
+  res.send('HELLO FROM MY API');
+});
 
 app.get('/posts', async (req, res) => {
   try {
-    const posts = await getAllPosts()
-    console.log('Posts from database:', posts); 
-    res.status(200).json(posts)
+    const posts = await getAllPosts();
+    console.log('Posts from database:', posts);
+    res.status(200).json(posts);
   } catch (error) {
-    console.error('Error al obtener los posts:', error)
-    res.status(500).send('Error en el servidor')
+    console.error('Error al obtener los posts:', error);
+    res.status(500).send('Error en el servidor');
   }
-})
+});
 
 app.get('/posts/:postId', async (req, res) => {
   try {
-    const post = await getById(req.params.postId)
+    const post = await getById(req.params.postId);
     if (post.length === 0) {
-      res.status(404).json({ message: 'Post no encontrado' })
+      res.status(404).json({ message: 'Post no encontrado' });
     } else {
-      res.status(200).json(post[0])
+      res.status(200).json(post[0]);
     }
   } catch (error) {
-    console.error('Error al obtener el post por ID:', error)
-    res.status(500).send('Error en el servidor')
+    console.error('Error al obtener el post por ID:', error);
+    res.status(500).send('Error en el servidor');
   }
-})
+});
 
 app.post('/posts', async (req, res) => {
   const {
-    title, content, author, category, tags,
-  } = req.body
+    title, content, author, genre,
+  } = req.body;
 
   try {
-    const message = 'Post created successfully! :)'
-    const result = await createPost(title, content, author, category, tags)
+    const message = 'Post created successfully! :)';
+    const result = await createPost(title, content, author, genre);
 
-    res.status(200).json({ message, result })
+    res.status(200).json({ message, result });
   } catch (error) {
-    console.error('Error al crear el post:', error)
-    res.status(500).send('Error en el servidor')
+    console.error('Error al crear el post:', error);
+    res.status(500).send('Error en el servidor');
   }
-})
+});
 
 app.put('/posts/:postId', async (req, res) => {
   const {
-    title, content, author, category, tags,
-  } = req.body
+    title, content, author, genre,
+  } = req.body;
 
   try {
-    const updatedPost = await updatePostById(title, content, author, category, tags, req.params.postId)
+    const updatedPost = await updatePostById(title, content, author, genre, req.params.postId);
     if (updatedPost.affectedRows === 0) {
-      res.status(404).json({ message: 'Post no encontrado' })
+      res.status(404).json({ message: 'Post no encontrado' });
     } else {
-      res.status(200).json(updatedPost)
+      res.status(200).json(updatedPost);
     }
   } catch (error) {
     console.error('Error al actualizar el post:', error)
